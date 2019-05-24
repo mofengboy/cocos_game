1d986673c350bb16f2e70d4def0ff0f6f7b7267e
mofeng <me@sunan.me>
2019-05-24 13:18:02 +0800

    架构完成

--- assets/Script/Index.js
+++ assets/Script/Index.js
@@ -48,6 +48,6 @@ cc.Class({
 
     //黑洞反复出现消失
     appear_hidden: function () {
-        this.node._children[1].children[1].opacity = Math.floor(Math.random() * 224 + 1);
+        this.node._children[0]._children[1].opacity = Math.floor(Math.random() * 224 + 1);
     }
 });
